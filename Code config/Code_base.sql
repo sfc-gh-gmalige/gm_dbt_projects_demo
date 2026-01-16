6c311cf5a31ffa309e1f7702520291d15eefcf64
sfc-gh-gmalige <gokula.malige@snowflake.com>
2026-01-16 09:53:16 +0000

    push dev sales dbt project code

---
+++ Code config/Code_base.sql
@@ -0,0 +1,207 @@
+USE ROLE SYSADMIN;  
+USE WAREHOUSE ACCOUNT_ADMIN_WH;
+
+ show parameters;
+ 
+CREATE OR REPLACE SCHEMA DBT_DEMO.DEV_SALES;
+CREATE OR REPLACE SCHEMA DBT_DEMO.QA_SALES;
+CREATE OR REPLACE SCHEMA DBT_DEMO.PROD_SALES;
+
+
+ALTER SCHEMA DBT_DEMO.DEV_SALES SET LOG_LEVEL = 'INFO';
+ALTER SCHEMA DBT_DEMO.DEV_SALES SET TRACE_LEVEL = 'ALWAYS';
+ALTER SCHEMA DBT_DEMO.DEV_SALES SET METRIC_LEVEL = 'ALL';
+
+ALTER SCHEMA DBT_DEMO.QA_SALES SET LOG_LEVEL = 'INFO';
+ALTER SCHEMA DBT_DEMO.QA_SALES SET TRACE_LEVEL = 'ALWAYS';
+ALTER SCHEMA DBT_DEMO.QA_SALES SET METRIC_LEVEL = 'ALL';
+
+ALTER SCHEMA DBT_DEMO.PROD_SALES SET LOG_LEVEL = 'INFO';
+ALTER SCHEMA DBT_DEMO.PROD_SALES SET TRACE_LEVEL = 'ALWAYS';
+ALTER SCHEMA DBT_DEMO.PROD_SALES SET METRIC_LEVEL = 'ALL';
+ 
+
+CREATE OR REPLACE FILE FORMAT DBT_DEMO.DEV_SALES.csv_ff 
+type = 'csv';
+
+CREATE OR REPLACE STAGE DBT_DEMO.DEV_SALES.s3load
+COMMENT = 'Quickstarts S3 Stage Connection'
+url = 's3://sfquickstarts/frostbyte_tastybytes/'
+file_format = DBT_DEMO.DEV_SALES.csv_ff;
+
+/*--
+ DEV_SALES zone table build 
+--*/
+
+-- country table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.country
+(
+    country_id NUMBER(18,0),
+    country VARCHAR(16777216),
+    iso_currency VARCHAR(3),
+    iso_country VARCHAR(2),
+    city_id NUMBER(19,0),
+    city VARCHAR(16777216),
+    city_population VARCHAR(16777216)
+) 
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- franchise table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.franchise 
+(
+    franchise_id NUMBER(38,0),
+    first_name VARCHAR(16777216),
+    last_name VARCHAR(16777216),
+    city VARCHAR(16777216),
+    country VARCHAR(16777216),
+    e_mail VARCHAR(16777216),
+    phone_number VARCHAR(16777216) 
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- location table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.location
+(
+    location_id NUMBER(19,0),
+    placekey VARCHAR(16777216),
+    location VARCHAR(16777216),
+    city VARCHAR(16777216),
+    region VARCHAR(16777216),
+    iso_country_code VARCHAR(16777216),
+    country VARCHAR(16777216)
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- menu table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.menu
+(
+    menu_id NUMBER(19,0),
+    menu_type_id NUMBER(38,0),
+    menu_type VARCHAR(16777216),
+    truck_brand_name VARCHAR(16777216),
+    menu_item_id NUMBER(38,0),
+    menu_item_name VARCHAR(16777216),
+    item_category VARCHAR(16777216),
+    item_subcategory VARCHAR(16777216),
+    cost_of_goods_usd NUMBER(38,4),
+    sale_price_usd NUMBER(38,4),
+    menu_item_health_metrics_obj VARIANT
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- truck table build 
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.truck
+(
+    truck_id NUMBER(38,0),
+    menu_type_id NUMBER(38,0),
+    primary_city VARCHAR(16777216),
+    region VARCHAR(16777216),
+    iso_region VARCHAR(16777216),
+    country VARCHAR(16777216),
+    iso_country_code VARCHAR(16777216),
+    franchise_flag NUMBER(38,0),
+    year NUMBER(38,0),
+    make VARCHAR(16777216),
+    model VARCHAR(16777216),
+    ev_flag NUMBER(38,0),
+    franchise_id NUMBER(38,0),
+    truck_opening_date DATE
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- order_header table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.order_header
+(
+    order_id NUMBER(38,0),
+    truck_id NUMBER(38,0),
+    location_id FLOAT,
+    customer_id NUMBER(38,0),
+    discount_id VARCHAR(16777216),
+    shift_id NUMBER(38,0),
+    shift_start_time TIME(9),
+    shift_end_time TIME(9),
+    order_channel VARCHAR(16777216),
+    order_ts TIMESTAMP_NTZ(9),
+    served_ts VARCHAR(16777216),
+    order_currency VARCHAR(3),
+    order_amount NUMBER(38,4),
+    order_tax_amount VARCHAR(16777216),
+    order_discount_amount VARCHAR(16777216),
+    order_total NUMBER(38,4)
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- order_detail table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.order_detail 
+(
+    order_detail_id NUMBER(38,0),
+    order_id NUMBER(38,0),
+    menu_item_id NUMBER(38,0),
+    discount_id VARCHAR(16777216),
+    line_number NUMBER(38,0),
+    quantity NUMBER(5,0),
+    unit_price NUMBER(38,4),
+    price NUMBER(38,4),
+    order_item_discount_amount VARCHAR(16777216)
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+-- customer loyalty table build
+CREATE OR REPLACE TABLE DBT_DEMO.DEV_SALES.customer_loyalty
+(
+    customer_id NUMBER(38,0),
+    first_name VARCHAR(16777216),
+    last_name VARCHAR(16777216),
+    city VARCHAR(16777216),
+    country VARCHAR(16777216),
+    postal_code VARCHAR(16777216),
+    preferred_language VARCHAR(16777216),
+    gender VARCHAR(16777216),
+    favourite_brand VARCHAR(16777216),
+    marital_status VARCHAR(16777216),
+    children_count VARCHAR(16777216),
+    sign_up_date DATE,
+    birthday_date DATE,
+    e_mail VARCHAR(16777216),
+    phone_number VARCHAR(16777216)
+)
+COMMENT = '{"origin":"sf_sit-is", "name":"tasty-bytes-dbt", "version":{"major":1, "minor":0}, "attributes":{"is_quickstart":1, "source":"sql"}}';
+
+/*--
+ DEV_SALES zone table load 
+--*/
+
+-- country table load
+COPY INTO DBT_DEMO.DEV_SALES.country
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/country/;
+
+-- franchise table load
+COPY INTO DBT_DEMO.DEV_SALES.franchise
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/franchise/;
+
+-- location table load
+COPY INTO DBT_DEMO.DEV_SALES.location
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/location/;
+
+-- menu table load
+COPY INTO DBT_DEMO.DEV_SALES.menu
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/menu/;
+
+-- truck table load
+COPY INTO DBT_DEMO.DEV_SALES.truck
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/truck/;
+
+-- customer_loyalty table load
+COPY INTO DBT_DEMO.DEV_SALES.customer_loyalty
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_customer/customer_loyalty/;
+
+-- order_header table load
+COPY INTO DBT_DEMO.DEV_SALES.order_header
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/order_header/;
+
+-- order_detail table load
+COPY INTO DBT_DEMO.DEV_SALES.order_detail
+FROM @DBT_DEMO.DEV_SALES.s3load/DEV_SALES_pos/order_detail/;
+
+-- setup completion note
+SELECT 'DBT_DEMO setup is now complete' AS note;
